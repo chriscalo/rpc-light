36dd3e657ac1754d5db25761f0022f0803e4ac83
Chris Calo <chris.calo@gmail.com>
2021-06-30 00:35:44 -0400

    Create server.test.js

---
+++ server.test.js
@@ -0,0 +1,40 @@
+const test = require("ava");
+const request = require("supertest");
+const { rpcService } = require("./rpc-server.js");
+
+test("exports a function", t => {
+  t.is(typeof rpcService, "function");
+});
+
+test("calling rpcService returns an express handler function", t => {
+  const handler = rpcService({
+    foo() {},
+  });
+  t.is(typeof handler, "function");
+});
+
+test("handler calls method with args, returns result as JSON", async t => {
+  t.plan(4);
+  
+  const methods = {
+    greet(name, exclaim = false) {
+      t.is(name, "Chris");
+      const punctuation = exclaim ? "!": "."
+      const message = `Hello, ${name}` + punctuation;
+      return {
+        message,
+      };
+    },
+  };
+  const handler = rpcService(methods);
+  const res = await request(handler).post("/").send({
+    path: ["greet"],
+    args: ["Chris", true],
+  });
+  
+  t.is(res.ok, true);
+  t.is(res.type, "application/json");
+  t.like(res.body, {
+    message: "Hello, Chris!",
+  });
+});
